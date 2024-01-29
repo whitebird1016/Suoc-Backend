6a0ac09430e24ad57b66750fe335bcf46c63aa7b
whitebird1016 <takus.superdev@gmail.com>
2024-01-30 02:15:41 +0900

    latest updated

--- routes/mint.route.js
+++ routes/mint.route.js
@@ -1,22 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-import mintController from "../controllers/mint.controller.js"
+import mintController from "../controllers/mint.controller.js";
 
 dotenv.config();
 const router = express.Router();
 
 router.post("/getSuocNFTMetadata", mintController.getSuocNFTMetadata);
 
-router.get("/getCurrentTier", mintController.getCurrentTier)
+router.get("/getCurrentTier", mintController.getCurrentTier);
 
-router.post("/getTierInfo", mintController.getTierInfo)
+router.post("/getTierInfo", mintController.getTierInfo);
 
-router.post("/getAddressInfo", mintController.getAddressInfo)
+router.post("/getAddressInfo", mintController.getAddressInfo);
 
-router.post("/createdNFT", mintController.createdNFT)
+router.post("/createdNFT", mintController.createdNFT);
 
-router.post("/getIsMintable", mintController.getIsMintable)
-
-router.post("/payForMint", mintController.payForMint)
+router.post("/getIsMintable", mintController.getIsMintable);
+router.post("/payForMint", mintController.payForMint);
 
 export default router;
